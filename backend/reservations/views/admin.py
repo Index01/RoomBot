6966df1266a37f0224c12f4ff53184e31aa84f76
t4l3r <tyler32bit@gmail.com>
2023-10-22 15:45:30 -0700

    Filtering and arranging metrics better.
        - headers added
        - filters verified

--- backend/reservations/views/admin.py
+++ backend/reservations/views/admin.py
@@ -65,7 +65,7 @@ class RoomCounts:
                                counts['allocated'],
                                counts['transfer'],
                                counts['orphan'])
-                remaining = Room.objects.filter(name_take3=room_type, is_available=True).count()
+            remaining = Room.objects.filter(name_take3=room_type, is_available=True).count()
 
             line = f"{room_type} room allocated: {counts['allocated']}, transfer: {counts['transfer']}, remaining: {remaining}, orphan: {counts['orphan']} (of {counts['available']} available)"
             logger.info(line)
@@ -534,7 +534,7 @@ def request_metrics(request):
 
         guest_unique = len(set([guest.email for guest in guessssts]))
         guest_count = Guest.objects.all().count()
-        guest_unplaced = len(guessssts.filter(room=None))
+        guest_unplaced = len(guessssts.filter(room=None, ticket__isnull=True))
  
         rooms_count = rooooms.count()
         rooms_occupied = rooooms.exclude(is_available=True).count()
@@ -550,7 +550,7 @@ def request_metrics(request):
         typz = set([room.name_take3 for room in rooooms])
         room_type_totals = dict(zip(typz, [len(rooooms.filter(name_take3=typ)) for typ in typz]))
         type_totals = {f'{k}_total'.replace(' ', '_'):v for k,v in room_type_totals.items()}
-        room_type_unoccupied = dict(zip(typz, [len(rooooms.filter(guest=None, name_take3=typ)) for typ in typz]))
+        room_type_unoccupied = dict(zip(typz, [len(rooooms.filter(is_available=True, name_take3=typ)) for typ in typz]))
         type_unocc = {f'{k}_unoccupied'.replace(' ', '_'):v for k,v in room_type_unoccupied.items()}
 
         metrics = {"guest_count": guest_count,
