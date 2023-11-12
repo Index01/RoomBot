edf3b05c6ab68700cf6f72cff80182227994ca91
t4l3r <tyler32bit@gmail.com>
2023-11-11 18:58:21 -0800

    Waittime styles
        - new css for waittimes
        - column rework
        - addming in the update time button.

--- frontend/src/pages/Waittime.js
+++ frontend/src/pages/Waittime.js
@@ -1,4 +1,4 @@
-import "../styles/RoombotAdmin.css";
+import "../styles/WaitTimes.css";
 import { TheTimers, HowLongTho } from "../components/waittimes.js";
 import React from "react";
 import { Toaster } from 'react-hot-toast';
@@ -7,10 +7,15 @@ export function Waittime() {
   return(
     <div className="componentContainer">
       <div className="AppHeader">
-	<img src="/roombaht_header.png" alt="RoomBaht9000" />
+	    <img src="/roombaht_header.png" alt="RoomBaht9000" />
       </div>
+
       <div className="DTApp">
         <HowLongTho />
+      </div>
+
+      <div className="AppNav">
+	      <a href="/waittime">Wait Times</a>
       </div>
     </div>
   );
@@ -20,12 +25,15 @@ export function WaittimeList() {
   return(
     <>
     <div className="componentContainer">
+
       <div className="AppHeader">
-	<img src="/roombaht_header.png" alt="RoomBaht9000" />
+          <img src="/roombaht_header.png" alt="RoomBaht9000" />
       </div>
+
       <div className="DTApp">Wait Times for the people because the people need to Wait
         <TheTimers />
       </div>
+
     </div>
     <Toaster/>
     </>
