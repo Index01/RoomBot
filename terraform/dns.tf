7dc79970850b24d8520dcb5273028f1560be7b27
Jonathan Freedman <jonafree@gmail.com>
2025-02-07 09:48:38 -0800

    until next year

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,18 +6,18 @@ resource "aws_route53_zone" "roombaht" {
   }
 }
 
-#resource "aws_route53_record" "dev" {
+# resource "aws_route53_record" "dev" {
+#   zone_id = aws_route53_zone.roombaht.id
+#   name = "dev.${var.domain}"
+#   type = "A"
+#   ttl = "300"
+#   records = [module.dev.public_ip]
+# }
+
+# resource "aws_route53_record" "prod" {
 #  zone_id = aws_route53_zone.roombaht.id
-#  name = "dev.${var.domain}"
+#  name = "${var.domain}"
 #  type = "A"
 #  ttl = "300"
-#  records = [module.dev.public_ip]
-#}
-
-resource "aws_route53_record" "prod" {
-  zone_id = aws_route53_zone.roombaht.id
-  name = "${var.domain}"
-  type = "A"
-  ttl = "300"
-  records = [module.prod.public_ip]
-}
+#  records = [module.prod.public_ip]
+# }
